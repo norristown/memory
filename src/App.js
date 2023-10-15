80e79c056e587044f9584e0f7c616adf80dffab3
norristown <jarvasity@gmail.com>
2023-10-15 03:02:53 -0400

    api works

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
-import { useState } from "react";
-
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+  function shuffleArray(array) {
+    return [...array].sort(() => Math.random() - 0.5);
+  }
+
+  const [apiItems, setApiItems] = useState([]);
+  const randomApiItems = shuffleArray(apiItems);
+
+  useEffect(function () {
+    async function fetchAPI() {
+      const arr = [];
+      for (let i = 1; i < 20; i++) {
+        const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        const data = await url.json();
+        const name = data.name;
+        const image = data.sprites.front_default;
+        const obj = { name, image };
+        arr.push(obj);
+      }
+      setApiItems(arr);
+    }
+    fetchAPI();
+  }, []);
   return (
-    <div className="App">
-      <ul>
-        {movies.map((movie) => (
-          <li>
-            <img src={movie.Poster} alt={`${movie.Title} poster`} />
-            <h3>{movie.Title}</h3>
-            <div>
-              <p>
-                <span>Year: </span>
-                <span>{movie.Year}</span>
-              </p>
-            </div>
-          </li>
-        ))}
-      </ul>
+    <div className="app">
+      <Header />
+      <CardList apiItems={apiItems} />
     </div>
   );
+}
+
+function CardList({ apiItems }) {
+  return (
+    <ul>
+      {apiItems.map((item) => (
+        <Card item={item} key={item.name} />
+      ))}
+    </ul>
+  );
+}
+
+function Card({ item }) {
+  return (
+    <li>
+      <img src={item.image} alt={`${item.name} poster`} />
+      <h3>{item.name}</h3>
+      <div>
+        <p>
+          <span>Text</span>
+        </p>
+      </div>
+    </li>
+  );
+}
+
+function Header() {
+  return <h1>Memory Card</h1>;
 }
